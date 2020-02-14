03db24a9bfa5dedc929a824b51280771b7c7b8ed
Chd Saglam <cihadsaglam@Chds-MacBook-Pro.local>
2020-02-14 09:18:23 +0100

    Build Control Component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
+
+const buildControls = (props) => (
+  <div></div>
+);
+
+export default buildControls;
