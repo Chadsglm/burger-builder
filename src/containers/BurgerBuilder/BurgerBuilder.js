6dcad57c34f092e4f1da4c2cf7b25f7c5feb65b9
Chd Saglam <cihadsaglam@Chds-MacBook-Pro.local>
2020-02-18 10:32:42 +0100

    Sending a POST Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-order';
+
 
 
 const INGREDIENT_PRICES = {
@@ -81,7 +83,28 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    // alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Chad Saglam',
+        address: {
+          street: 'Teststrasse 12',
+          zipCode: '8302',
+          country: 'Switzerland'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+    axios.post('/order.json', order)
+         .then(response => {
+           console.log(response);
+         })
+         .catch(error => {
+           console.log(error);
+         })
   }
 
   render() {
