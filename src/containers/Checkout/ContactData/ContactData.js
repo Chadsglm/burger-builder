2b8feebf7e5400ae9b3e099b14dabd23bd240ddd
Chd Saglam <cihadsaglam@Chds-MacBook-Pro.local>
2020-03-06 08:24:29 +0100

    Working on Order Actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -90,8 +90,7 @@ class ContactData extends Component {
             valid: true
         }
     },
-    formIsValid: false,
-    loading: false
+    formIsValid: false
   }
 
   orderHandler = (event) => {
@@ -191,7 +190,7 @@ class ContactData extends Component {
       </form>
     );
 
-    if(this.state.loading) {
+    if(this.props.loading) {
       form = <Spinner />;
     }
 
@@ -207,13 +206,14 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   }
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
   };
 };
 
